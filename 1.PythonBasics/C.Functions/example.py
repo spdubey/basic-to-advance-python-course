8e3dd829fe7f3876163e12c106230922ce082f26
Abhishek Dubey <33081271+spdubey@users.noreply.github.com>
2021-12-08 10:54:41 +0530

    Add files via upload

---
+++ 1.PythonBasics/C.Functions/example.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    """
+    This function adds two numbers
+    """
+    return a + b
+
+def multiply(a, b):
+    """
+    This function multiply two numbers
+    """
+    return a * b
+
+def subtract(a, b):
+    """
+    This function subtract two numbers
+    """
+    return a - b
+
+def division(a, b):
+    """
+    This function divides two numbers
+    """
+    return a / b
